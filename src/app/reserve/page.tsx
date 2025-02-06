8f5f1ddc70fc501cc964f2992e87f255af782434
hwangy1 <yuna.hwang@hva.nl>
2025-02-06 09:05:21 +0900

    [ADD] datepicker in RESERVE ing...

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -2,13 +2,43 @@
 import Link from "next/link";
 import Topbar from "../../../components/topbar";
 import { User, Time, Menu, Notice } from "../../../components/icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
 
 export default function Page() {
+  const [date, setDate] = useState(new Date());
+  const [disabledDates, setDisabledDates] = useState([]);
   const [menu, setMenu] = useState({
     option: [] as number[],
     request: "",
   });
+
+  const getAllTuesdays = (startDate, endDate) => {
+    const dates = [];
+    const currentDate = new Date(startDate);
+
+    while (currentDate <= endDate) {
+      if (currentDate.getDay() === 2) {
+        dates.push({
+          startDate: new Date(currentDate),
+          endDate: new Date(currentDate),
+        });
+      }
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dates;
+  };
+
+  useEffect(() => {
+    const startDate = new Date();
+    const endDate = new Date(
+      startDate.getFullYear(),
+      startDate.getMonth() + 1,
+      0
+    );
+    setDisabledDates(getAllTuesdays(startDate, endDate));
+  }, []);
 
   const handleOptionChange = (value: number) => {
     setMenu((prev) => ({
@@ -31,7 +61,7 @@ export default function Page() {
   return (
     <div className="flex flex-col h-screen">
       <Topbar title={"Étoile Nail"} />
-      <main className="flex flex-col min-h-screen gap-y-4 pt-8 px-4 bg-gray-50">
+      <main className="flex flex-col gap-y-8 pt-8 px-4 bg-gray-50">
         <div className="flex flex-col gap-y-8">
           <div className="flex flex-col gap-y-4">
             <div className="flex gap-x-1.5">
@@ -93,7 +123,18 @@ export default function Page() {
                 날짜와 시간을 선택해주세요
               </div>
             </div>
-            <div className="font-sans text-gray-700">캘린더</div>
+            <Datepicker
+              useRange={false}
+              asSingle={true}
+              value={date}
+              onChange={(date) => setDate(date)}
+              minDate={new Date()}
+              disabledDates={disabledDates}
+              primaryColor={"cyan"}
+              // toggleClassName={
+              //   "absolute right-0 h-full px-3 rounded-r-lg text-cyan-500"
+              // }
+            />
             <div className="font-sans text-gray-700">시간</div>
           </div>
           <div className="flex flex-col gap-y-4">
@@ -108,7 +149,7 @@ export default function Page() {
                 <input
                   type="checkbox"
                   onChange={() => handleOptionChange(1)}
-                  className="h-4 w-4 checked:bg-rose-300/90"
+                  className="h-4 w-4 checked:bg-cyan-500"
                 />
                 젤 제거
               </label>
@@ -116,7 +157,7 @@ export default function Page() {
                 <input
                   type="checkbox"
                   onChange={() => handleOptionChange(2)}
-                  className="size-4 checked:bg-rose-300/90"
+                  className="size-4 checked:bg-cyan-500"
                 />
                 원컬러
               </label>
@@ -124,7 +165,7 @@ export default function Page() {
                 <input
                   type="checkbox"
                   onChange={() => handleOptionChange(3)}
-                  className="size-4 checked:bg-rose-300/90"
+                  className="size-4 checked:bg-cyan-500"
                 />
                 간단아트
               </label>
@@ -132,7 +173,7 @@ export default function Page() {
                 <input
                   type="checkbox"
                   onChange={() => handleOptionChange(4)}
-                  className="size-4 checked:bg-rose-300/90"
+                  className="size-4 checked:bg-cyan-500"
                 />
                 풀아트
               </label>
@@ -140,7 +181,7 @@ export default function Page() {
                 <input
                   type="checkbox"
                   onChange={() => handleOptionChange(5)}
-                  className="size-4 checked:bg-rose-300/90"
+                  className="size-4 checked:bg-cyan-500"
                 />
                 기본케어
               </label>
@@ -148,10 +189,9 @@ export default function Page() {
                 <div className="font-sans font-medium text-gray-700">
                   요청사항
                 </div>
-                <input
-                  type="textarea"
+                <textarea
                   onChange={handelRequestChange}
-                  className="flex rounded-lg h-[100px] border border-gray-200 px-2 py-1"
+                  className="flex rounded-lg border border-gray-200 px-2 py-1"
                 />
               </div>
             </div>
@@ -167,10 +207,10 @@ export default function Page() {
             <div className="font-sans text-gray-700">공지사항2</div>
           </div>
         </div>
-        <div className="flex w-full">
+        <div className="flex w-full mb-8">
           <Link
             href="/show"
-            className="flex w-full items-center justify-center rounded-full bg-rose-300/90 font-sans font-medium text-white px-4 py-2"
+            className="flex w-full items-center justify-center rounded-full bg-cyan-500/90 font-sans font-medium text-white px-4 py-2"
           >
             예약 완료
           </Link>
